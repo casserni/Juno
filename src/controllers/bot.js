a686c396dc27bc3f16d0dd2371dc38d61a43fe4e
casserni@yahoo.com <nicholascassera@Nicholass-MBP.fios-router.home>
2017-05-08 19:09:37 -0400

    Middleware

--- src/controllers/bot.js
+++ src/controllers/bot.js
@@ -1,6 +1,9 @@
 import builder from 'botbuilder';
 import connector from './connector';
+import utuMiddleware from '../middlewares/utu';
 
 const bot = new builder.UniversalBot(connector);
+
+bot.use(utuMiddleware);
 
 export default bot
